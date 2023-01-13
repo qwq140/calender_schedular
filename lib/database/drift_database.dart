c9e74555a2c5e14c48de4cd4318315c9a3c52e02
qwq140 <qwq140@gmail.com>
2023-01-13 14:02:45 +0900

    drift database 설정

---
+++ lib/database/drift_database.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+
+import 'package:drift/drift.dart';
+import 'package:todo_app/model/schedules.dart';
+
+import 'package:drift/native.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
+
+part 'drift_database.g.dart';
+
+@DriftDatabase(
+    tables : [
+      Schedules,
+    ],
+)
+class LocalDatabase extends _$LocalDatabase {
+
+  LocalDatabase() : super(_openConnection());
+
+  Stream<List<Schedule>> watchSchedules(DateTime date) => (select(schedules)..where((tbl) => tbl.date.equals(date))).watch();
+  Future<int> createSchedule(SchedulesCompanion data) => into(schedules).insert(data);
+  Future<int> removeSchedule(int id) => (delete(schedules)..where((tbl) => tbl.id.equals(id))).go();
+
+  @override
+  int get schemaVersion => 1;
+}
+
+// 드리프트 데이터베이스 객체는 부모 생성자에 LazyDatabase를 필수로 넣어줘야 한다.
+// LazyDatabase 객체에는 데이터베이스를 생성할 위치에 대한 정보를 입력해주면 된다.
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    // 데이터베이스 파일을 저장할 폴더
+    final dbDirectory = await getApplicationDocumentsDirectory();
+    final file = File(join(dbDirectory.path, 'db.sqlite'));
+    return NativeDatabase(file);
+  });
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/database/drift_database.dart';
 import 'package:todo_app/ui/home_screen.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:get_it/get_it.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await initializeDateFormatting();
+
+  final database = LocalDatabase(); // 데이터베이스 생성
+
+  GetIt.I.registerSingleton<LocalDatabase>(database);
 
   runApp(const MyApp());
 }
